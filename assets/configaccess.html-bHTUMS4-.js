9cccacc8fffd7c1cc0c272465f0d038f6ffe6b46
rupraph <rupraph@users.noreply.github.com>
2026-01-28 17:21:13 +0000

    deploy: rupraph/pipo-site-vuepress@b9a250ae4143fc4fc93284834dd9553ed7a65648

--- assets/configaccess.html-bHTUMS4-.js
+++ assets/configaccess.html-bHTUMS4-.js
@@ -1,4 +1,4 @@
-import{_ as t,c as i,a as e,d as n,o as a}from"./app-D7ui5v-u.js";const c={};function r(s,o){return a(),i("div",null,[e(` ::: warning Prototype
+import{_ as t,c as i,a as e,d as n,o as a}from"./app-C2OV6Oi5.js";const c={};function r(s,o){return a(),i("div",null,[e(` ::: warning Prototype
 The modules are still under development. Hence these directions are only valid for protytpe testing.
 Note: some features are not yet implemented, and some are not mature yet.
 
@@ -19,4 +19,4 @@ The network IP address of the Pipo is displayed in the top Bar as &quot;STAIP&qu
     - enter your credentials
     - click save
     - if succesfull, Pipo will restart, and after a few seconds, the Wifi Led on the pipo should start slowly pulsing. This means pipo is connected and running
-  - If pipo can connect to previous wifi, it will do automatically `)])}const p=t(c,[["render",r]]),u=JSON.parse('{"path":"/manual/quick-start-guides/configaccess.html","title":"WiFi & Page access","lang":"en-US","frontmatter":{"title":"WiFi & Page access","order":2},"git":{"createdTime":1769620415000,"updatedTime":1769620415000,"contributors":[{"name":"Rup","username":"Rup","email":"rupertrenevier@hotmail.com","commits":1,"url":"https://github.com/Rup"}]},"readingTime":{"minutes":2.06,"words":617},"filePathRelative":"manual/quick-start-guides/configaccess.md","localizedDate":"January 28, 2026"}');export{p as comp,u as data};
+  - If pipo can connect to previous wifi, it will do automatically `)])}const p=t(c,[["render",r]]),u=JSON.parse('{"path":"/manual/quick-start-guides/configaccess.html","title":"WiFi & Page access","lang":"en-US","frontmatter":{"title":"WiFi & Page access","order":2},"git":{"createdTime":1769620827000,"updatedTime":1769620827000,"contributors":[{"name":"Rup","username":"Rup","email":"rupertrenevier@hotmail.com","commits":1,"url":"https://github.com/Rup"}]},"readingTime":{"minutes":2.06,"words":617},"filePathRelative":"manual/quick-start-guides/configaccess.md","localizedDate":"January 28, 2026"}');export{p as comp,u as data};
